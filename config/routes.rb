afa52b11a0fc8b4d877a15ccdebc0b4968f573d7
Beekun Muhammad Mozaffar <mozaf2905@gmail.com>
2021-11-09 23:05:27 +0400

    The link between list and bookings has been done including booknow

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'vehicles#index'
   resources :vehicles do
-    resources :bookings, only: [:create, :new]
+   resources :bookings
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
- resources :bookings, only: [:index, :destroy, :edit, :update, :show]
+  resources :bookings, only: [:index, :destroy, :edit, :update, :show]
+
 end
