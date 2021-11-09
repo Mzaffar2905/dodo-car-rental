c42e46f633d2ae70c56f082d15c1360013c33310
DushveerSumaruth <dushveer11@gmail.com>
2021-11-09 17:48:38 +0400

    The views for booking has been completed

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,18 +1,37 @@
 class BookingsController < ApplicationController
-end
+  def index
+    @bookings = Booking.all
+  end
+  def new
+    @vehicle = Vehicle.find(params[:vehicle_id])
+    @booking = Booking.new
+  end
 
-def new
+  def create
+    @vehicle = Vehicle.find(params[:vehicle_id])
+    @booking = Booking.new(bookings_params)
+    @booking.vehicle = @vehicle
+    @booking.user = current_user
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render :new
+    end
+  end
 
-end
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.user = current_user
+    if @booking.destroy
+      redirect_to bookings_path
+    else
+      render :new
+    end
+  end
 
-def create
-
-end
-
-def destroy
-
-end
-
-def index
+  private
+  def bookings_params
+    params.require(:booking).permit(:booking, :return)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'vehicles#index'
-  resources :vehicles 
+  resources :vehicles do
+    resources :bookings, only: [:create, :new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
+ resources :bookings, only: [:index, :destroy, :edit, :update, :show]
 end
