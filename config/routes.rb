a378e80ac7c860cb6d2b2b9b02c4c69614a0ff1b
DushveerSumaruth <dushveer11@gmail.com>
2021-11-10 11:14:51 +0400

    created a homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'vehicles#index'
+  root to: 'pages#home'
   resources :vehicles do
    resources :bookings
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
