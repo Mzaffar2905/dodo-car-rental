7f6d1bba0f686d608d255b261cba92b68f3743f6
Beekun Muhammad Mozaffar <mozaf2905@gmail.com>
2021-11-10 15:24:12 +0400

    The updates done on homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :vehicles do
-   resources :bookings
+   resources :bookings, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
   resources :bookings, only: [:index, :destroy, :edit, :update, :show]
