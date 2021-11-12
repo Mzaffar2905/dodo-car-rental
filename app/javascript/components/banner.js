a96ec599bdff45f40cdd3aa90b586558bf3df348
Beekun Muhammad Mozaffar <mozaf2905@gmail.com>
2021-11-12 10:57:31 +0400

    Changes on list of vehicles and homepage

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,10 +2,11 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["Find our list of vehicles !"],
+    strings: ["Find our list of vehicles !", "And your booking list"],
     typeSpeed: 80,
     loop: true
   });
 }
 
 export { loadDynamicBannerText };
+
